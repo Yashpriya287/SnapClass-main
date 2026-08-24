a55a3b8ef11a881e9029d57bf8861634c833d2ad
Yash Priya <yashpriyadwivedi@gmail.com>
2026-08-24 14:53:41 +0530

    Update code

--- src/components/dailog_share_subject.py
+++ src/components/dailog_share_subject.py
@@ -3,7 +3,7 @@ import segno
 import io
 @st.dialog("Share Class Link")
 def share_subject_dailog(subject_name, subject_code):
-    app_domain="snapclass-main.streamlit.app"
+    app_domain="snapclass-ypd.streamlit.app"
     join_url=f"{app_domain}/?join-code={subject_code}"
 
     st.header("Scan to Join")
